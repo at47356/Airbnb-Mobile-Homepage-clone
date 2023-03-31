7741a61921d905275d128346e76b25bf858b98f8
at47356 <101987474+at47356@users.noreply.github.com>
2023-03-31 12:31:45 +0100

    Complete 'navbar' and start on hero

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
 import './App.css'
+import Navb from './nav'
 
 function App() {
   return (
     <div className="App">
-      
+      <Navb/>
+      <img src="photo-grid.png" alt="photogrid" className="photogrid"  />
     </div>
   )
 }
---
+++ src/nav.jsx
@@ -0,0 +1,10 @@
+
+export default function Navb() {
+    return (
+        <div className="navbar-container">
+            <img src="airbnb-logo.png" alt="image" className="airbnblogo"/>
+        </div>
+    )
+}
+
+// export default Navb
