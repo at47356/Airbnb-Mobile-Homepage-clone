75ce6d1e7f405eb4fd8e718f4f8bf53b52a51e44
at47356 <101987474+at47356@users.noreply.github.com>
2023-04-11 16:42:13 +0100

    Add Databse to take props from

--- src/App.jsx
+++ src/App.jsx
@@ -5,20 +5,27 @@ import './App.css'
 import Navb from './nav'
 import Hero from './Hero'
 import Card from './Card'
+import data from './data'
+
+
 
 function App() {
+  const cards = data.map(card => {
+    return <Card
+      title = {card.title}
+      price = {card.price}
+      img = {card.coverImg}
+      rating = {card.stats.rating}
+      reviews = {card.stats.reviewCount}
+      location = {card.location}
+    />
+  })
+
   return (
     <div className="App">
       <Navb/>
       <Hero/>
-      <Card
-        img="./katie-zaferes.png"
-        rating="5.0"
-        reviews={6}
-        location="USA"
-        title="Life Lessons with Katie Zaferes"
-        price={136}
-      />
+      {cards}
     </div>
   )
 }
